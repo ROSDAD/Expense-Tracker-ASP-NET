972f7143a6c2d3521b783c35869ebb8fc5b4f43e
ROSDAD <dadlani.r@northeastern.edu>
2024-07-08 15:32:48 -0400

    First commit, project created, done with category model

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mime;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        // Foreign Key
+        public int CategoryId { get; set; }
+
+        // Navigation Property
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "varchar(75)")]
+        public string? Note { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
